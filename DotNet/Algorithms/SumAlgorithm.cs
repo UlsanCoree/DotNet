6f98524af15f4a4cdd2b311fc13f1005a10bcf75
Kim SeungHyun <korafa@naver.com>
2024-03-27 06:57:10 +0900

    초기 커밋.

---
+++ DotNet/Algorithms/SumAlgorithm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class SumAlgorithm
+{
+    static void Main()
+    {
+        int[] scores = { 100, 75, 65, 95, 90, 40, 85, 100 };
+        int sum = 0;
+
+        for (int i = 0; i < scores.Length; i++)
+        {
+            if (scores[i] >= 80)
+                sum += scores[i];
+        }
+
+        Console.WriteLine($"{scores.Length}명의 점수중 80점 이상점수의 합은: {sum}");
+    }
+}
