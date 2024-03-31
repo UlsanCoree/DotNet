00efcc812ce4cd9c7c2bda626723d93f0021161c
Kim SeungHyun <korafa@naver.com>
2024-03-31 21:26:39 +0900

    3-31

---
+++ DotNet/Algorithms/FindMode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FindModeValue
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 3, 3, 3, 3, 5, 5, 5,5,5,5, 2, 2, 2, 2, 2, 7 };
+        int mode = FindMode(numbers);
+
+        Console.WriteLine($"최빈값: {mode}");
+    }
+
+    private static int FindMode(int[] numbers)
+    {
+        Dictionary<int, int> prequency = new Dictionary<int, int>();
+
+        int maxCount = 0;
+        int mode = numbers[0];
+
+        foreach (var num in numbers)
+        {
+            if (prequency.ContainsKey(num))
+            {
+                prequency[num]++;
+            }
+            else
+            {
+                prequency[num] = 1;
+            }
+
+            if (prequency[num] > maxCount)
+            {
+                maxCount = prequency[num];
+                mode = num;
+            }
+        }
+
+        return mode;
+    }
+}
