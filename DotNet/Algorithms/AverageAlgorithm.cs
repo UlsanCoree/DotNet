d29e6895ade44e9866755681813a1e3a139fd61f
Kim SeungHyun <korafa@naver.com>
2024-04-01 15:01:48 +0900

    4-1

---
+++ DotNet/Algorithms/AverageAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Average
+{
+    static void Main()
+    {
+        int[] numData = { 90, 88, 40, 75, 59, 98, 71 };
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < numData.Length; i++)
+        {
+                sum += numData[i];
+                count++;
+        }
+
+        double average = (double)sum / count;
+
+        Console.WriteLine($"평균은: {average}");
+    }
+}
