7fcf99b24e46754fac6888b85dc81d12656080c9
Kim SeungHyun <korafa@naver.com>
2024-04-02 12:34:42 +0900

    4-2

---
+++ DotNet/Algorithms/MaxAlgorithm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class MaxAlgorithm
+{
+    static void Main()
+    {
+        int[] numbers = { -10, -20, -5, -6, -3, -1 , -200 ,150};
+
+        int maxNum = numbers[0];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > maxNum)
+            {
+                maxNum = numbers[i];
+            }
+        }
+
+        Console.WriteLine($"최댓값 : {maxNum}");
+    }
+}
