ee13d534a964c2365ce0fbea3e54f7255d26798e
Kim SeungHyun <korafa@naver.com>
2024-04-09 06:40:17 +0900

    4-9

---
+++ DotNet/Algorithms/AverageExeptMaxAndMin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+class AverageExeptMaxAndMin
+{
+    static void Main()
+    {
+        int[] scores = { 10, 20, 30, 40, 50 };
+
+        int count = 0;
+        int sum = 0;
+        int min = scores.Min();
+        int max = scores.Max();
+
+        for (int i = 0; i < scores.Length; i++)
+        {
+            if (scores[i] != min && scores[i] != max)
+            {
+                sum += scores[i];
+                count++;
+            }
+        }
+        var average = count > 0 ? (double)sum / count : 0;
+
+        Console.WriteLine($"{average}");
+    }
+}
---
+++ DotNet/Algorithms/NearAlgorithm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class NearAlgorithm
+{
+    static void Main()
+    {
+        int min = int.MaxValue;
+
+        int[] numbers = { 10, 20, 30, 27, 17 };
+        int target = 25;
+        int nearNum = default;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int abs = Math.Abs(numbers[i] - target);
+            if (abs < min)
+            {
+                min = abs;
+                nearNum = numbers[i];
+            }
+        }
+
+        var minimum = numbers.Min(m => Math.Abs(m - target));
+        var closest = numbers.First(n => Math.Abs(n - target) == minimum);
+
+        Console.WriteLine($"{target} 에 가장 가까운 수는 {closest}, 차이는{minimum}");
+        Console.WriteLine($"{target} 에 가장 가까운 수는 {nearNum}, 차이는{min}");
+    }
+}
