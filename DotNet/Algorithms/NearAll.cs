48c4cfe7974dfefdecbc0efe5ab172d6f7d5d1ab
Kim SeungHyun <korafa@naver.com>
2024-04-11 17:07:18 +0900

    4-11

---
+++ DotNet/Algorithms/NearAll.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class NearAll
+{
+    static void Main()
+    {
+        int[] data = { 10, 20, 30, 23, 27 };
+        int target = 25;
+        int min = int.MaxValue;
+
+        List<int> nears = new List<int>();
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (Math.Abs(data[i]-target) < min)
+            {
+                min = Math.Abs(data[i] - target);
+            }
+        }
+
+        Console.WriteLine(min);
+
+        for (int i = 0;i < data.Length; i++)
+        {
+            if (Math.Abs(data[i] - target) == min)
+            {
+                nears.Add(data[i]);
+            }
+        }
+
+        foreach (var nearValue in nears)
+        {
+            Console.WriteLine(nearValue);
+        }
+    }
+}
